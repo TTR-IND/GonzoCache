dd1fdd760af06eda5562e77003808a1939b3d292
Josh~ <joshwheatstone@gmail.com~>
2026-07-28 12:11:57 +0100

    UPDATE

--- gonzocache.c
+++ gonzocache.c
@@ -1,6 +1,33 @@
 /*
- * gonzocache.c -- login-time app preloader, a companion project to
- * detritusd (https://github.com/YOUR_GITHUB/detritusd).
+ * ╔══════════════════════════════════════════════════════════════╗
+ * ║                                                              ║
+ * ║   ████████╗████████╗██████╗       ██╗███╗   ██╗██████╗       ║
+ * ║   ╚══██╔══╝╚══██╔══╝██╔══██╗      ██║████╗  ██║██╔══██╗      ║
+ * ║      ██║      ██║   ██████╔╝      ██║██╔██╗ ██║██║  ██║      ║
+ * ║      ██║      ██║   ██╔══██╗      ██║██║╚██╗██║██║  ██║      ║
+ * ║      ██║      ██║   ██║  ██║      ██║██║ ╚████║██████╔╝      ║
+ * ║      ╚═╝      ╚═╝   ╚═╝  ╚═╝      ╚═╝╚═╝  ╚═══╝╚═════╝       ║
+ * ║                                                              ║
+ * ║       Torfaen Technology Research — IND                      ║
+ * ║       Copyright © 2026                                       ║
+ * ║       Licensed under Apache License 2.0                      ║
+ * ║                                                              ║
+ * ╚══════════════════════════════════════════════════════════════╝
+ *
+ * detritus.c -- Event-Driven Memory Manager for Linux
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  *
  * Two modes, one binary:
  *
